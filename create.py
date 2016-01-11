90099f6326adc9d54ebee32a70ef0c58d0a5d27f
kagers <ka.gershfeld@gmail.com>
2016-01-11 11:47:15 -0500

    ff

--- create.py
+++ create.py
@@ -14,22 +14,20 @@ c.execute(q)
 q = "create table users(id integer, name text, password text)"
 c.execute(q)
 
-q = "create table posts(id integer, jasondata text, file text, time timestamp default current_timestamp, uid integer)"
+q = "create table posts(id integer, jasondata text, file text, time timestamp default current_timestamp, uid integer, yelpid text)"
 c.execute(q)
 
 q = "create table comments(id integer, content text, pid integer, uid integer)"
 c.execute(q)
 
-#q = "create table restaurants(id integer, name text, location text)"
-#c.execute(q)
+q = "create table restaurants(yelpid text, name text, location text, rating text, phone text)"
+c.execute(q)
 
 conn.commit()
 
 '''
 jasonSS
 SSSSSSSSSSS
-restaurant
-location
 price
 likes
 tags
